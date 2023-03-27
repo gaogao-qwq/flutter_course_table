24c84d44a1f4fd9330c3457be86ecd9f97ab50e1
gaogao-qwq <gaogaoqwq@gmail.com>
2023-03-27 13:37:16 +0800

    Refactor file directory

---
+++ lib/pages/import_page/select_semester_dialog.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course_table_demo/internal/handlers/response_handlers.dart';
+
+class SelectSemesterDialog extends StatefulWidget{
+  final List<SemesterInfo> semesterList;
+
+  const SelectSemesterDialog({
+    super.key,
+    required this.semesterList,
+  });
+
+  @override
+  State<SelectSemesterDialog> createState() => _SelectSemesterDialogState();
+}
+
+class _SelectSemesterDialogState extends State<SelectSemesterDialog> {
+  int? selectedYearIndex;
+  String? selectedSemester;
+
+  @override
+  void initState() {
+    selectedYearIndex = widget.semesterList.length - 1;
+    selectedSemester = widget.semesterList[selectedYearIndex!].semesterId1;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: const Text("Choose semester"),
+      children: <Widget>[
+        Column(
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                DropdownButton(
+                  items: getYearItems(),
+                  value: selectedYearIndex.toString(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedYearIndex = (value ?? 0) as int?;
+                    });
+                  },
+                ),
+                DropdownButton(
+                  items: getSemesterItems(),
+                  value: selectedSemester,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedSemester = value ?? "";
+                    });
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, selectedSemester);
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll<Color>(Colors.green),
+                  ),
+                  child: const Text("Import"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  List<DropdownMenuItem<String>> getYearItems() {
+    var items = <DropdownMenuItem<String>>[];
+    for (int i = 0; i < widget.semesterList.length; i++) {
+      items.add(DropdownMenuItem(value: i.toString(), child: Text("${widget.semesterList[i].value}学年"),));
+    }
+    return items;
+  }
+
+  List<DropdownMenuItem<String>> getSemesterItems() {
+    var items = <DropdownMenuItem<String>>[];
+    items.add(DropdownMenuItem(value: widget.semesterList[selectedYearIndex!].semesterId1, child: const Text("第1学期"),));
+    items.add(DropdownMenuItem(value: widget.semesterList[selectedYearIndex!].semesterId2, child: const Text("第2学期"),));
+    return items;
+  }
+}
