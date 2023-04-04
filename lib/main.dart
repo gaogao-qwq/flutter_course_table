685459ed00d3722a6e01527b81045cdbd748790a
gaogao-qwq <gaogaoqwq@gmail.com>
2023-04-04 15:19:58 +0800

    Chore: Add support for dark theme It doesn't work for now though...

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_course_table_demo/pages/home_page/home_page.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future main() async{
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
+  await ScreenUtil.ensureScreenSize();
   runApp(CourseTableApp(prefs: prefs));
 }
 
-class CourseTableApp extends StatelessWidget {
+class CourseTableApp extends StatefulWidget {
   final SharedPreferences prefs;
   const CourseTableApp({
     super.key,
@@ -16,7 +19,52 @@ class CourseTableApp extends StatelessWidget {
   });
 
   @override
+  State<StatefulWidget> createState() => _CourseTableAppState();
+}
+
+class _CourseTableAppState extends State<CourseTableApp> {
+  ThemeMode themeMode = ThemeMode.system;
+
+  bool get useLightMode {
+    switch (themeMode) {
+      case ThemeMode.system:
+        return SchedulerBinding.instance.window.platformBrightness == Brightness.light;
+      case ThemeMode.light:
+        return true;
+      case ThemeMode.dark:
+        return false;
+    }
+  }
+
+  void handleBrightnessChange(bool useLightMode) {
+    setState(() {
+      themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    themeMode = ThemeMode.dark;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CourseTableHomePage(prefs: prefs));
+    return MaterialApp(
+      theme: ThemeData(
+        colorSchemeSeed: const Color(0xff6750a4),
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: const Color(0xff6750a4),
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: CourseTableHomePage(
+        prefs: widget.prefs,
+        handleBrightnessChange: handleBrightnessChange,
+      ),
+    );
   }
 }
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_table_demo/pages/home_page/change_current_course_table_dialog.dart';
 import 'package:flutter_course_table_demo/pages/home_page/course_table_widget_builder.dart';
+import 'package:flutter_course_table_demo/pages/home_page/delete_stored_course_table_dialog.dart';
 import 'package:flutter_course_table_demo/pages/import_page/import_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class CourseTableHomePage extends StatefulWidget {
   final SharedPreferences prefs;
+  final void Function(bool useLightMode) handleBrightnessChange;
+
   const CourseTableHomePage({
     super.key,
     required this.prefs,
+    required this.handleBrightnessChange,
   });
 
   @override
@@ -15,11 +20,12 @@ class CourseTableHomePage extends StatefulWidget {
 }
 
 class _CourseTableHomePageState extends State<CourseTableHomePage> {
-  String? currCourseTableName;
+  late String currCourseTableName;
 
   @override
   void initState() {
-    currCourseTableName = "0";
+    // currCourseTableName = "2022-2023学年第2学期课表";
+    currCourseTableName = "";
     super.initState();
   }
 
@@ -28,6 +34,9 @@ class _CourseTableHomePageState extends State<CourseTableHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Course Table Demo'),
+        actions: [
+          _BrightnessButton(handleBrightnessChange: widget.handleBrightnessChange),
+        ],
       ),
       drawer: Drawer(
         child: ListView(
@@ -44,14 +53,67 @@ class _CourseTableHomePageState extends State<CourseTableHomePage> {
               onTap: () async {
                 Navigator.pop(context);
                 final tmp = await Navigator.push(context, MaterialPageRoute(builder: (context) => ImportTablePage(prefs: widget.prefs)));
+                if (tmp == null || tmp.isEmpty) return;
                 setState(() { currCourseTableName = tmp; });
               },
             ),
             ListTile(
               title: const Text("Change Table"),
-              onTap: () {
+              onTap: () async {
                 Navigator.pop(context);
-                setState(() { currCourseTableName = ""; });
+                Set<String> keys = widget.prefs.getKeys();
+                if (keys.isEmpty) {
+                  showDialog(context: context, builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Oops"),
+                      content: const Text(
+                          "You haven't import any course table yet"
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                            onPressed: () => {Navigator.of(context).pop()},
+                            child: const Text("OK")
+                        )
+                      ],
+                    );
+                  });
+                  return;
+                }
+                final tmp = await Navigator.push(context,
+                  DialogRoute(context: context, builder: (context) => ChangeCurrentCourseTable(prefs: widget.prefs, currCourseTableName: currCourseTableName))
+                );
+                if (tmp == null) return;
+                if (currCourseTableName != tmp) { setState(() { currCourseTableName = tmp; }); }
+              },
+            ),
+            ListTile(
+              title: const Text("Delete course table"),
+              onTap: () async {
+                Navigator.pop(context);
+                Set<String> keys = widget.prefs.getKeys();
+                if (keys.isEmpty) {
+                  showDialog(context: context, builder: (context) {
+                    return AlertDialog(
+                      title: const Text("Oops"),
+                      content: const Text(
+                          "You haven't import any course table yet"
+                      ),
+                      actions: <Widget>[
+                        TextButton(
+                            onPressed: () => {Navigator.of(context).pop()},
+                            child: const Text("OK")
+                        )
+                      ],
+                    );
+                  });
+                  return;
+                }
+                final tmp = await Navigator.push(context,
+                  DialogRoute(context: context, builder: (context) => DeleteStoredCourseTable(prefs: widget.prefs, currCourseTableName: currCourseTableName))
+                );
+                if (tmp == null) return;
+                if (currCourseTableName == tmp) setState(() { currCourseTableName = ""; });
+                widget.prefs.remove(tmp);
               },
             ),
             ListTile(
@@ -68,7 +130,53 @@ class _CourseTableHomePageState extends State<CourseTableHomePage> {
           ],
         ),
       ),
-      body: CourseTableWidget(courseTableName: currCourseTableName!, prefs: widget.prefs),
+      body: (currCourseTableName.isEmpty)
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Text("There is nothing here, select or import a course table first"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final tmp = await Navigator.push(context, MaterialPageRoute(builder: (context) => ImportTablePage(prefs: widget.prefs)));
+                      if (tmp == null || tmp.isEmpty) return;
+                      setState(() { currCourseTableName = tmp; });
+                    },
+                    child: const Text("Import"),
+                  ),
+                ],
+              )
+            )
+          : CourseTableWidget(courseTableName: currCourseTableName, courseTableRow: 12, prefs: widget.prefs),
+    );
+  }
+}
+
+class _BrightnessButton extends StatelessWidget {
+  final Function handleBrightnessChange;
+  final bool showTooltipBelow;
+
+  const _BrightnessButton({
+    required this.handleBrightnessChange,
+    this.showTooltipBelow = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isBright = Theme.of(context).brightness == Brightness.light;
+    return Tooltip(
+      preferBelow: showTooltipBelow,
+      message: 'Toggle brightness',
+      child: IconButton(
+        icon: isBright
+            ? const Icon(Icons.dark_mode_outlined)
+            : const Icon(Icons.light_mode_outlined),
+        onPressed: () => handleBrightnessChange(!isBright),
+      ),
     );
   }
 }
