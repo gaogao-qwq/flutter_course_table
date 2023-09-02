4c14248dd72afe4b62e8ace73beaa977830ba4ec
gaogao-qwq <gaogaoqwq@gmail.com>
2023-09-02 16:50:05 +0800

    Chore: Add License comment

--- lib/pages/import_page/import_from_crawler.dart
+++ lib/pages/import_page/import_from_crawler.dart
@@ -1,3 +1,19 @@
+// A simple course table app
+// Copyright (C) 2023 Zhihao Zhou
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_course_table/internal/database/course_table_repository.dart';
 import 'package:flutter_course_table/internal/handlers/response_handlers.dart';
